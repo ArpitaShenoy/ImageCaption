3e5a71902f8226a32f8c14e372efbe6325602f37
Arpita Shenoy <arpitashenoy@Arpitas-MacBook-Air.local>
2024-10-27 17:23:14 +0530

    Add new files

---
+++ app.py
@@ -0,0 +1,36 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+import streamlit as st
+import os
+import google.generativeai as genai
+from PIL import Image
+
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
+model=genai.GenerativeModel("gemini-1.5-pro")
+
+def get_gemini_respone(image):
+    response=model.generate_content(["Caption this image",image]) # input -> describe what you want to do with the image
+    return response.text
+
+st.set_page_config(page_title="Image Caption Generator")
+
+st.header("Generate a caption for your Image")
+
+# input=st.text_input("What do you want to do with the image?", key="input")
+
+uploaded_file=st.file_uploader("Upload an image..", type=['jpg','jpeg','png'])
+
+image=""
+
+if uploaded_file:
+    image=Image.open(uploaded_file)
+    st.image(image, caption="Uploaded Image", use_column_width=True)
+
+submit=st.button("Generate")
+
+if submit:
+    response=get_gemini_respone(image)
+    # st.subheader("Here's what I think...")
+    st.write(response)
